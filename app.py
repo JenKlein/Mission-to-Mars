b63d082aa24c77daecad55f989242ce41b23f5af
Jennifer Klein <jenniferklein@Jennifers-MacBook-Air.local>
2021-05-01 08:14:51 -0400

    Flask routes initiated

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+#initialize flask app
+app = Flask(__name__)
+
+#use flask_pymonogo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+##Set Up App Routes
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all() ## access database
+   mars.update({}, mars_data, upsert=True) ##update database  .update(query_parameter, data, options)
+   return redirect('/', code=302)
+
+if __name__ == "__main__":
+   app.run()
